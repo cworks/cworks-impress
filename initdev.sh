8b6d07cae551b212ea472ef3dc697f1a6ac5257a
cworks <cworkscode@gmail.com>
2014-09-20 14:11:50 -0500

    initial commit

---
+++ initdev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+npm install grunt --save-dev
+npm install grunt-connect-proxy --save-dev
+npm install grunt-cli --save-dev
+npm install grunt-contrib-clean --save-dev
+npm install grunt-contrib-concat --save-dev
+npm install grunt-contrib-connect --save-dev
+npm install grunt-contrib-copy --save-dev
+npm install grunt-contrib-jshint --save-dev
+npm install grunt-contrib-uglify --save-dev
+npm install grunt-contrib-watch --save-dev
+npm install grunt-text-replace --save-dev
+npm install load-grunt-tasks --save-dev
+npm install time-grunt --save-dev
